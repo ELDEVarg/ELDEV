cd142a3dc68402702ebaf77ad5ca23e83f347c30
ELDEVarg <100127200+ELDEVarg@users.noreply.github.com>
2022-02-23 09:21:56 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -2,3 +2,24 @@ window.onload= function(){
 	$('#onload').fadeOut();
 	$('body').removeClass('hidden')
 }
+
+function mostrarInfo(){
+	document.getElementById('info-text').style.display = 'grid';
+}
+function cerrarInfo(){
+	document.getElementById('info-text').style.display = 'none';
+}
+
+function mostrarIncluye(){
+	document.getElementById('incluye-text').style.display = 'grid';
+}
+function cerrarIncluye(){
+	document.getElementById('incluye-text').style.display = 'none';
+}
+
+function mostrarPyn(){
+	document.getElementById('pyn-text').style.display = 'grid';
+}
+function cerrarPyn(){
+	document.getElementById('pyn-text').style.display = 'none';
+}
